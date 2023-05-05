1d549fb610de20be9098ab008c3afd2870e64ba6
dgengtek <dgengtek@users.noreply.github.com>
2023-05-05 15:31:13 +0200

    Remove ale

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -153,16 +153,6 @@ return require('packer').startup(function(use)
     },
   }
 
-  -- Load on a combination of conditions: specific filetypes or commands
-  -- Also run code after load (see the "config" key)
-  -- syntax analyzer
-  use {
-    'w0rp/ale',
-    -- ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
-    -- cmd = 'ALEEnable',
-    -- config = 'vim.cmd[[ALEEnable]]'
-  }
-
   -- Post-install/update hook with neovim command
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use 'nvim-treesitter/nvim-treesitter-context'
