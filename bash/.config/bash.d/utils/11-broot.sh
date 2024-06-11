0dc6ffb53b451284bfc29f45fc0705ac4449887d
dgengtek <dgengtek@users.noreply.github.com>
2024-06-11 23:19:13 +0200

    Refactor broot no mode

--- bash/.config/bash.d/utils/11-broot.sh
+++ bash/.config/bash.d/utils/11-broot.sh
@@ -1,33 +1,32 @@
 #!/usr/bin/env bash
 if ! hash broot 2>&1 | logger -t bashrc -p user.info; then
-  return 1
+	return 1
 fi
 
-br() {
-    local cmd cmd_file code
-    cmd_file=$(mktemp)
-    if broot --outcmd "$cmd_file" "$@"; then
-        cmd=$(<"$cmd_file")
-        command rm -f "$cmd_file"
-        eval "$cmd"
-    else
-        code=$?
-        command rm -f "$cmd_file"
-        return "$code"
-    fi
+function br {
+	local cmd cmd_file code
+	cmd_file=$(mktemp)
+	if broot --outcmd "$cmd_file" "$@"; then
+		cmd=$(<"$cmd_file")
+		command rm -f "$cmd_file"
+		eval "$cmd"
+	else
+		code=$?
+		command rm -f "$cmd_file"
+		return "$code"
+	fi
 }
 
 alias bo="broot --conf '$HOME/.config/broot/conf.hjson;$HOME/.config/broot/select.toml'"
 
-
-unalias z 2> /dev/null
+unalias z 2>/dev/null
 z() {
-  local dir=$(broot --conf "$HOME/.config/broot/conf.hjson;$HOME/.config/broot/selectd.toml")
-  if [[ -z "$dir" ]]; then
-    builtin cd || return
-  fi
-  if [[ -f "$dir" ]]; then
-    dir=$(dirname "$dir")
-  fi
-  cd "$dir"
+	local dir=$(broot --conf "$HOME/.config/broot/conf.hjson;$HOME/.config/broot/selectd.toml")
+	if [[ -z "$dir" ]]; then
+		builtin cd || return
+	fi
+	if [[ -f "$dir" ]]; then
+		dir=$(dirname "$dir")
+	fi
+	cd "$dir"
 }
